3ef9ebda1f860fa154817182525aea423364e07e
valentine OO <varlomxg@gmail.com>
2024-06-13 01:33:12 +0100

    first commit

---
+++ api/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json([
+    {
+      id: "1",
+      title: "Book Review: The Bear & Nightingale Dreams",
+    },
+    {
+      id: "2",
+      title: "Game Review: Ninja Brazilian Diamond",
+    },
+    {
+      id: "3",
+      title: "Show Review: Alice in Wonderland",
+    },
+  ]);
+});
+
+app.listen(4000, () => {
+  console.log("Listening for requests on port 4000...");
+});
